fbcb7588215f8b6b940f4a5b32e8f6aed08499b1
horobchenko <gorobchenkotatyana16@gmail.com>
2024-08-23 10:24:35 +0300

    Merge remote-tracking branch 'origin/master'

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,11 @@ from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite+pysqlite:///battery.db"
-app.config['SECRET_KEY'] = 'jguitc6747990h'
+app.config['SECRET_KEY'] = ''
 app.config['MQTT_BROKER_URL'] = 'io.adafruit.com' # use the free broker from HIVEMQ
 app.config['MQTT_BROKER_PORT'] = 1883  # default port for non-tls connection
-app.config['MQTT_USERNAME'] = 'Hor'  # set the username here if you need authentication for the broker
-app.config['MQTT_PASSWORD'] = "aio_rWhm93j8NmzYixdrDGNk9Cr98ua0" # set the password here if the broker demands authentication
+app.config['MQTT_USERNAME'] = ''  # set the username here if you need authentication for the broker
+app.config['MQTT_PASSWORD'] = "" # set the password here if the broker demands authentication
 #app.config['MQTT_KEEPALIVE'] = 120  # set the time interval for sending a ping to the broker to 5 seconds
 app.config['MQTT_TLS_ENABLED'] = False  # set TLS to disabled for testing purposes
 db = SQLAlchemy()
